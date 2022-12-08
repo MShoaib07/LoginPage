2877a1b9d337abb19a2b8006b5f70da03aa5f357
Shoaib <mohammadshaikh2000@gmail.com>
2022-12-08 15:07:05 +0530

    Intendent the code

--- loginPage/LoginViewController.swift
+++ loginPage/LoginViewController.swift
@@ -19,23 +19,23 @@ class LoginViewController: UIViewController {
     }
 
     @IBAction func btnLogin(_ sender: UIButton) {
-        if let email = txtEmail.text, let password = txtPassword.text{ 
-                    if !email.validateEmailId() {
-                        openAlert(title: "Alert", message: "Email Address not found.", alertStyle: .alert, actionTitles: ["Okay"], actionStyles: [.default], actions: [{ _ in
-                            print("Okay Clicked!")
-                        }])
-                    }else if !password.validatePassword() {
-                        openAlert(title: "Alert", message: "Please enter valid password.", alertStyle: .alert, actionTitles: ["Okay"], actionStyles: [.default], actions: [{ _ in
-                            print("Okay Clicked!")
-                        }])
-                    }else{
-                        //Navigation - Home Screen
-                    }
-                }else{
-                    openAlert(title: "Alert", message: "Please enter details.", alertStyle: .alert, actionTitles: ["Okay"], actionStyles: [.default], actions: [{ _ in
-                        print("Okay Clicked!")
-                    }])
-                }
+        if let email = txtEmail.text, let password = txtPassword.text{
+            if !email.validateEmailId() {
+                openAlert(title: "Alert", message: "Email Address not found.", alertStyle: .alert, actionTitles: ["Okay"], actionStyles: [.default], actions: [{ _ in
+                    print("Okay Clicked!")
+                }])
+            }else if !password.validatePassword() {
+                openAlert(title: "Alert", message: "Please enter valid password.", alertStyle: .alert, actionTitles: ["Okay"], actionStyles: [.default], actions: [{ _ in
+                    print("Okay Clicked!")
+                }])
+            }else{
+                //Navigation - Home Screen
             }
+        }else{
+            openAlert(title: "Alert", message: "Please enter details.", alertStyle: .alert, actionTitles: ["Okay"], actionStyles: [.default], actions: [{ _ in
+                print("Okay Clicked!")
+            }])
+        }
+    }
 
 }
